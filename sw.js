2e00bacae4860afa8e905e32bc528dbcf4af2014
leonelriversg-cloud <leonel.rivers.g@gmail.com>
2025-10-05 21:45:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,8 @@
-// Service Worker simple para caching
-const CACHE_NAME = 'reloj-24h';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
+// Service Worker simple
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
